97c529792bf5950ce8920057d6ce680ee4123e9f
Soul <soulctf@gmail.com>
2019-02-10 22:13:26 -0500

    Setup

--- SVM.rs
+++ SVM.rs
@@ -1,8 +1,30 @@
 struct VM {
-	registers: [i32; 8],
+	registers: [u32; 8],
 	pc: usize,
+	program: Vec<u8>;
 }
 
 impl VM {
 
+	pub fn run(&mut self) {
+		
+		loop {
+			if self.pc >= self.program.len() {
+				break;
+			}
+			match self.serve_opcode() {
+				OpCode::NOP => {
+					return;
+				},
+				OpCode::HLT => {
+					return;
+				},
+				_ => { 
+					print!("You broke it :(");
+					return;
+				}
+			}
+		}
+
+	}
 }
